5b0176e58d41c771b172c06b39242c9bdaa8d773
imSai-py <sai.lak.2004920@gmail.com>
2025-06-08 19:17:42 +0530

    Add functionality to count letters, digits, and special characters in a string

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,4 @@
+#1:to find the modified string
 user = input("Enter a string: ")
 modified_char = []
 
@@ -10,3 +11,25 @@ for i, char in enumerate(user):
 modified_char = "".join(modified_char)
 
 print('modified string: ', modified_char)
+
+print('-'*50)
+
+#2:to find the count of letters, digits and special characters
+user = input("Enter a string: ")
+
+letter_count = 0
+digit_count = 0
+special_count = 0
+
+for i, char in enumerate(user):
+    if char.isalpha():
+        letter_count += 1
+    elif char.isdigit():
+        digit_count += 1
+    else:
+        special_count += 1
+
+print('letter count: ', letter_count)
+print('digit count: ', digit_count)
+print('special count: ', special_count)
+
