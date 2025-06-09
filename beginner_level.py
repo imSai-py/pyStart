676a99d5bd115482bedb00ce1c4b77fc927aa8f4
imSai-py <sai.lak.2004920@gmail.com>
2025-06-09 22:15:34 +0530

    Add functions for displaying a tree pattern and greeting with parameters

--- beginner_level.py
+++ beginner_level.py
@@ -27,3 +27,32 @@ for value in some_list:
     else:
         seen.add(value)
 print("Duplicates found:", duplicates)
+
+# first def function:(a concept used to avoid DRY - Don't Repeat Yourself) it provides code resusability
+list =[
+    [0,0,0,1,0,0,0],
+    [0,0,1,1,1,0,0],
+    [0,1,1,1,1,1,0],
+    [1,1,1,1,1,1,1],
+    [0,0,0,1,0,0,0],
+    [0,0,0,1,0,0,0]
+]
+
+def show_tree():
+    for row in list:
+        for pixels in row:
+            if (pixels == 1):
+                print('*', end='')
+            else:
+                print(' ', end='')
+        print()
+show_tree()
+show_tree()
+show_tree()
+show_tree()
+
+# def function with parameters:
+def greetings(name, emoji):
+    print(f'Hello {name}{emoji}')
+# calling the function with parameters using arguments:
+greetings('Sai', '😊')
