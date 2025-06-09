ce6e503b2ce2fb51a929192d4cf2c73a8b18431b
imSai-py <sai.lak.2004920@gmail.com>
2025-06-09 21:23:21 +0530

    Add functionality to find duplicates in a list using both counting and set methods

--- beginner_level.py
+++ beginner_level.py
@@ -5,3 +5,25 @@ while True:
     if response == "bye":
         print("Goodbye!")
         break
+
+# finding duplicate without set:
+some_list = ['a','b','b','c','d','d','e']
+duplicates = []
+
+for values in some_list:
+    if some_list.count(values) > 1 and values not in duplicates:
+        duplicates.append(values)
+print("Duplicates found:", duplicates)
+
+# finding duplicate with set:
+some_list = ['a','b','b','c','d','d','e']
+
+seen = set()
+duplicates = set()
+
+for value in some_list:
+    if value in seen:
+        duplicates.add(value)
+    else:
+        seen.add(value)
+print("Duplicates found:", duplicates)
